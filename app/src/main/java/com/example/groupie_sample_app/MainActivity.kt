e3857be0373d1d38006ffc9ee9cbc2c2973a54d8
momo <mmko5110@gmail.com>
2019-05-01 18:55:26 +0900

    MainActivityに記述

--- app/src/main/java/com/example/groupie_sample_app/MainActivity.kt
+++ app/src/main/java/com/example/groupie_sample_app/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.groupie_sample_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.ViewHolder
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val groupAdapter = GroupAdapter<ViewHolder>()
+        findViewById<RecyclerView>(R.id.recycler_view).adapter = groupAdapter
+
+        val items = listOf("りんご", "みかん", "ぶどう")
+        items.forEach { item ->
+            groupAdapter.add(ListItem(item))
+        }
     }
 }
